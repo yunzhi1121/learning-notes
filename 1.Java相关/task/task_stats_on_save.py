0c69393402435a30ee1ae5d7f6a91c642106371f
yunzhi1121 <yunzhi1121@qq.com>
2025-05-20 17:37:34 +0100

    update ignore

--- 1.Java相关/task/task_stats_on_save.py
+++ 1.Java相关/task/task_stats_on_save.py
@@ -1,7 +1,7 @@
 import re
 
 # 使用绝对路径，替换成你自己的真实路径
-file_path = r"A:\01 找工作\01 线上实习\retail-management-system\retail-management-system\docs\0.学习笔记\1.Java相关\task\task_list.md"
+file_path = r"A:\01 找工作\06 学习笔记\1.Java相关\task\task_list.md"
 
 with open(file_path, "r", encoding="utf-8") as f:
     content = f.read()
